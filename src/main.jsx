3ed378accd344aa28a9d4b0656cf04334d5a4a91
Ihor_Koniaha <konyaga.igor10@gmail.com>
2024-02-11 15:18:44 +0200

    start

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { store } from './redux/store.js';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/car_rental">
+      <BrowserRouter basename="/car-rental">
         <App />
       </BrowserRouter>
     </Provider>
